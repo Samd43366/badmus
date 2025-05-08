bf70f5da2f8edc5acd609b1b608a88ba85261a03
Your Name <your.email@example.com>
2025-05-08 12:23:51 +0100

    i added new features and add 11.py file

---
+++ 11th.py
@@ -0,0 +1,10 @@
+l1 = [3, 6, 9, 12, 15, 18, 21]
+
+# Define the second list of numbers
+l2 = [4, 8, 12, 16, 20, 24, 28]
+
+# Create a new list by combining:
+l3 = [l1[1::2] + l2[0::2]]
+
+# Print the newly created list
+print(l3)
